82bbaaddb3b56810aea6c1745b8d42105a6969f0
Luka Knezic <luka@giscloud.com>
2026-07-08 03:02:24 +0200

    Add comment to clarify early return in blog template validation script

--- containers/blog/scripts/validate_blog_templates.sh
+++ containers/blog/scripts/validate_blog_templates.sh
@@ -5,6 +5,7 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 BLOGS="$SCRIPT_DIR/../templates/blogs"
 
 if [ -d "$BLOGS" ] && [ -z "$(find "$BLOGS" -maxdepth 1 -mindepth 1 -not -name '.gitkeep')" ]; then
+    # No blog templates found, early return
     exit 0
 fi
 
